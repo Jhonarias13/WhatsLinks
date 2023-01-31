0278bd8cd7e5e89895f52050784e4ce1328e1c5e
Jhonarias13 <jhonarias1322@gmail.com>
2023-01-31 18:25:21 -0500

    Initial Commit

---
+++ Whatlinks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Whatlinks
+//
+//  Created by Jhon Freiman Arias on 31/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whatlinks/WhatlinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatlinksApp.swift
+//  Whatlinks
+//
+//  Created by Jhon Freiman Arias on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatlinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
